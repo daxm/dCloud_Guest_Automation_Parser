5791159d56f287a0f420188ef99d81b495483fdc
rsp2k <rpm@malloys.us>
2017-05-15 17:49:50 -0600

    pep8 variable names

--- update_passwords.py
+++ update_passwords.py
@@ -9,43 +9,36 @@ This file's source is from https://github.com/daxm/dCloud_Guest_Automation_Parse
 
 import xmltodict
 import json
-from lxml import etree
+import lxml
 import os
-import sys
 
 # Fix the path variable to match your setup.
-pathtosessionfile = '.'
+session_file_path = '.'
 #pathtosessionfile = '/dcloud'
-sessionfilename = 'session.xml'
-
-pathtousermappingfile = '.'
-#pathtousermappingfile = '/etc/guacamole/'
-usermappingfilename = 'user-mapping.xml'
+session_file_name = 'session.xml'
 
 # Load the session file, convert from XML to an OrderedDict to a Dict.
-sessionfile = os.path.join(pathtosessionfile, sessionfilename)
-try:
-    with open(sessionfile, 'r') as file:
-        dcloudsession = json.loads(json.dumps(xmltodict.parse(file.read())))
-except:
-    print("Error: Cannot open %s.  Exiting..." % (sessionfile))
-    exit(1)
+session_file = os.path.join(session_file_path, session_file_name)
+
+with open(session_file, 'r') as file:
+    dcloud_session = json.loads(json.dumps(xmltodict.parse(file.read())))
 
 # I need the Session ID and the "oob" URL as they are needed to access the lab.
-sessionid = dcloudsession['session']['id']
-ooburl = dcloudsession['session']['translations']['translation']['name']
+session_id = dcloud_session['session']['id']
+oob_url = dcloud_session['session']['translations']['translation']['name']
 
-# Read in the user-mapping.xml file and update the guacamole users' password to the sessionid.
-usermappingfile = os.path.join(pathtousermappingfile, usermappingfilename)
-try:
-    tree = etree.parse(usermappingfile)
-    root = tree.getroot()
-    for child in root:
-        child.attrib['password'] = sessionid
+user_mapping_file_path = '.'
+#pathtousermappingfile = '/etc/guacamole/'
+user_mapping_file_name = 'user-mapping.xml'
 
-    # Save the updated user-mapping.xml file
-    tree = etree.ElementTree(root)
-    tree.write(usermappingfile)
-except:
-    print("Error: Cannot open %s.  Exiting..." % (usermappingfile))
-    exit(1)
+# Read in the user-mapping.xml file and update the guacamole users' password to the session_id.
+user_mapping_file = os.path.join(user_mapping_file_path, user_mapping_file_name)
+
+tree = lxml.etree.parse(user_mapping_file)
+root = tree.getroot()
+for child in root:
+    child.attrib['password'] = session_id
+
+# Save the updated user-mapping.xml file
+tree = lxml.etree.ElementTree(root)
+tree.write(user_mapping_file)
